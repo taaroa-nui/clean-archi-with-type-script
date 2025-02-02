68b1e26435b2d4dcb6595db4dbf5c5c0a6c79a9b
Pierre <richaud.pierre@outlook.fr>
2025-02-02 08:09:37 +0100

    initial commit

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
 import express from 'express'
-import serverConfig from './frameworks/webserver/server'
+import { serverConfig } from './src/frameworks/webserver'
+import routesIndex from './src/frameworks/webserver/routes/routesIndex'
+import expressConfig from './src/frameworks/webserver/expressConfig'
 
 const app = express()
-serverConfig(app)
+serverConfig(app).startServer()
+expressConfig(app)
+routesIndex(app)
 
 export default app
